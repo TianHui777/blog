cb5ff39f2e46b525b47d421d4ba210019636a743
TianHui777 <tianhui@mail.ynu.edu.cn>
2022-12-17 19:19:58 +0800

    统一异常处理

--- tianhui-blog/src/main/java/com/tianhui/controller/BlogLoginController.java
+++ tianhui-blog/src/main/java/com/tianhui/controller/BlogLoginController.java
@@ -2,8 +2,11 @@ package com.tianhui.controller;
 
 import com.tianhui.entity.ResponseResult;
 import com.tianhui.entity.User;
+import com.tianhui.enums.AppHttpCodeEnum;
+import com.tianhui.exception.SystemException;
 import com.tianhui.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,11 @@ public class BlogLoginController {
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user) {
+
+        // 如果没有携带用户名，则抛出异常
+        if (!StringUtils.hasText(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
 
         return blogLoginService.login(user);
     }
