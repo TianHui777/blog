3ab4a0c0156204be357687995edd276b02f1bd00
TianHui777 <tianhui@mail.ynu.edu.cn>
2023-01-16 14:39:36 +0800

    更新

--- tianhui-blog/src/main/java/com/tianhui/controller/CommentController.java
+++ tianhui-blog/src/main/java/com/tianhui/controller/CommentController.java
@@ -5,10 +5,7 @@ import com.tianhui.entity.Comment;
 import com.tianhui.entity.ResponseResult;
 import com.tianhui.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
@@ -19,24 +16,24 @@ public class CommentController {
     private CommentService commentService;
 
     // 获取所有的文章评论
-    @RequestMapping("/commentList")
+    @GetMapping("/commentList")
     public ResponseResult commentList(Long articleId, Integer pageNum, Integer pageSize) {
 
-        return commentService.commentList(SystemConstants.ARTICLE_COMMENT,articleId, pageNum, pageSize);
+        return commentService.commentList(SystemConstants.ARTICLE_COMMENT, articleId, pageNum, pageSize);
 
     }
 
 
     @PostMapping
 
-    public ResponseResult addComment(@RequestBody Comment comment){
+    public ResponseResult addComment(@RequestBody Comment comment) {
 
-       return commentService.addComment(comment);
+        return commentService.addComment(comment);
     }
 
     // 获取所有的文章评论
-    @RequestMapping("/linkCommentList")
-    public ResponseResult linkCommentList( Integer pageNum, Integer pageSize) {
+    @GetMapping("/linkCommentList")
+    public ResponseResult linkCommentList(Integer pageNum, Integer pageSize) {
 
         return commentService.commentList(SystemConstants.LINK_COMMENT, null, pageNum, pageSize);
 
